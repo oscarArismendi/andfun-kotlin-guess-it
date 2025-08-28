d2aee8060ab71ee776ece0f505b8a6cfbf54b8ea
oscarArismendi <oscarfernandoarismendigalvis@gmail.com>
2025-08-28 18:26:09 -0500

    chore: add GameViewModel class

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -17,11 +17,14 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
@@ -30,6 +33,8 @@ import com.example.android.guesstheword.databinding.GameFragmentBinding
  * Fragment where the game is played
  */
 class GameFragment : Fragment() {
+
+    private lateinit var viewModel: ViewModel
 
     // The current word
     private var word = ""
@@ -53,7 +58,9 @@ class GameFragment : Fragment() {
                 false
         )
 
-        // TODO (04) Create and initialize a GameViewModel, using ViewModelProvider; Add a log
+        Log.i("GameFragment","Called ViewModelProviders.of!")
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+
         // statement
 
         resetList()
