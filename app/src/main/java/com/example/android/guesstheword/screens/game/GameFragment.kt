5f9088abf738d43b182c14637ea33d3d4b1bcde3
oscarArismendi <oscarfernandoarismendigalvis@gmail.com>
2025-10-14 21:02:34 -0500

    feat: Use Data Binding to handle click listeners

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -51,26 +51,17 @@ class GameFragment : Fragment() {
 
         // Get the viewmodel
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
-
-        // TODO (03) Pass the GameViewModel into the data binding - then you can remove the
-        // OnClickListener setup from here
-        binding.correctButton.setOnClickListener {
-            viewModel.onCorrect()
-        }
-        binding.skipButton.setOnClickListener {
-            viewModel.onSkip()
-        }
-
+        binding.gameViewModel = viewModel
         /** Setting up LiveData observation relationship **/
-        viewModel.word.observe(this, Observer { newWord ->
+        viewModel.word.observe(viewLifecycleOwner, Observer { newWord ->
             binding.wordText.text = newWord
         })
 
-        viewModel.score.observe(this, Observer { newScore ->
+        viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
             binding.scoreText.text = newScore.toString()
         })
 
-        viewModel.currentTime.observe(this, Observer { newTime ->
+        viewModel.currentTime.observe(viewLifecycleOwner, Observer { newTime ->
             binding.timerText.text = DateUtils.formatElapsedTime(newTime)
 
         })
