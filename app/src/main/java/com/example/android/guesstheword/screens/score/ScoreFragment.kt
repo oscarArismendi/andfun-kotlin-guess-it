d21780cf4ce8e1cbc5dc6c7d449902861833bf89
oscarArismendi <oscarfernandoarismendigalvis@gmail.com>
2025-10-19 18:47:54 -0500

    feat: Use data binding with LiveData

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -58,8 +58,7 @@ class ScoreFragment : Fragment() {
                 .get(ScoreViewModel::class.java)
 
         binding.scoreViewModel = viewModel
-        // TODO (05) Call binding.setLifecycleOwner and remove the score observer
-
+        binding.setLifecycleOwner(this)
         // Add observer for score
         viewModel.score.observe(this, Observer { newScore ->
             binding.scoreText.text = newScore.toString()
